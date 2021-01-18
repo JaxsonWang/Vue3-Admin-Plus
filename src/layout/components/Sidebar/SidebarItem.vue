01731df7779f79eb1c0a880027c9dc1f5329883f
王瑾 <i@iiong.com>
2021-01-18 16:56:33 +0800

    [21.01.18] 更新 Vue3 架子

--- src/layout/components/Sidebar/SidebarItem.vue
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -1,32 +1,33 @@
 <template>
   <div v-if="!item.hidden">
-    <template v-if="hasOneShowingChild(item.children, item) && (!onlyOneChild.children || onlyOneChild.noShowingChildren) && !item.alwaysShow">
-      <app-link v-if="onlyOneChild.meta" :to="resolvePath(onlyOneChild.path)">
-        <el-menu-item :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
-          <item :icon="onlyOneChild.meta.icon||(item.meta&&item.meta.icon)" :title="onlyOneChild.meta.title" />
+    <template v-if="item.children && Array.isArray(item.children) && hasChildRoute(item.children)">
+      <el-submenu ref="subMenu" :index="resolvePath(item.path)" popper-append-to-body>
+        <template #title>
+          <item v-if="item.meta" :icon="item.meta && item.meta.icon" :title="item.meta.title" />
+        </template>
+        <sidebar-item
+          v-for="child in item.children"
+          :key="child.path"
+          :is-nest="true"
+          :item="child"
+          :base-path="resolvePath(child.path)"
+          class="nest-menu"
+        />
+      </el-submenu>
+    </template>
+    <template v-else>
+      <app-link :to="item.path">
+        <el-menu-item :index="resolvePath(item.path)" :class="{'submenu-title-noDropdown':!isNest}">
+          <item :icon="item.meta.icon || (item.meta && item.meta.icon)" :title="item.meta.title" />
         </el-menu-item>
       </app-link>
     </template>
-
-    <el-submenu v-else ref="subMenu" :index="resolvePath(item.path)" popper-append-to-body>
-      <template #title>
-        <item v-if="item.meta" :icon="item.meta && item.meta.icon" :title="item.meta.title" />
-      </template>
-      <sidebar-item
-        v-for="child in item.children"
-        :key="child.path"
-        :is-nest="true"
-        :item="child"
-        :base-path="resolvePath(child.path)"
-        class="nest-menu"
-      />
-    </el-submenu>
   </div>
 </template>
 
 <script>
 import path from 'path'
-import { defineComponent, reactive } from 'vue'
+import { defineComponent } from 'vue'
 import Item from './Item'
 import AppLink from './Link'
 import { isExternal } from '@/utils/validate'
@@ -52,31 +53,11 @@ export default defineComponent({
     }
   },
   setup(props) {
-    let onlyOneChild = reactive({})
-    const hasOneShowingChild = (children = [], parent) => {
-      const showingChildren = children.filter(item => {
-        if (item.hidden) {
-          return false
-        } else {
-          // Temp set(will be used if only has one showing child)
-          console.log(item)
-          onlyOneChild = item
-          return true
-        }
+    const hasChildRoute = (children = []) => {
+      const showChildren = children.filter(item => {
+        return !item.hidden
       })
-
-      // When there is only one child router, the child router is displayed by default
-      if (showingChildren.length === 1) {
-        return true
-      }
-
-      // Show parent if there are no child router to display
-      if (showingChildren.length === 0) {
-        onlyOneChild = { ...parent, path: '', noShowingChildren: true }
-        return true
-      }
-
-      return false
+      return showChildren.length > 0
     }
 
     const resolvePath = routePath => {
@@ -90,8 +71,7 @@ export default defineComponent({
     }
 
     return {
-      onlyOneChild,
-      hasOneShowingChild,
+      hasChildRoute,
       resolvePath
     }
   }
