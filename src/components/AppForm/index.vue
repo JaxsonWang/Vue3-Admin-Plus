6b04b94b66e7bb671e9aaa22f98430cbc03e9bad
王瑾 <i@iiong.com>
2021-03-18 18:46:56 +0800

    feat:add object el-form module

--- src/components/AppForm/index.vue
+++ src/components/AppForm/index.vue
@@ -8,115 +8,231 @@
 
 <template>
   <el-form
-    v-bind="appConfig.formAttr"
+    v-bind="appConfig.formAttrs"
     :model="appForm"
     ref="formRef"
     class="app-form-container"
   >
     <el-form-item
       v-for="(item, index) in appConfig.formList"
-      v-bind="item.labelAttr"
+      v-bind="item.labelAttrs"
       :key="index"
     >
-      <el-input
-        v-if="item.type === 'input'"
-        v-model="appForm[item.key]"
-        v-bind="item.formAttr"
-      />
       <el-radio-group
-        v-else-if="item.type === 'radio'"
+        v-if="item.type === 'radio'"
         v-model="appForm[item.key]"
-        v-bind="item.formAttr"
+        v-bind="item.formAttrs"
+        v-on="item.formEvents"
       >
-        <el-radio
-          v-for="(rdo, rid) in item.options"
-          v-bind="rdo.attr"
-          :key="rid"
-        >
-          {{ rdo.title }}
-        </el-radio>
+        <template v-if="item.isButton">
+          <el-radio-button
+            v-for="(rdo, rid) in item.options"
+            v-bind="rdo.attrs"
+            v-on="rdo.events"
+            :key="rid"
+          >
+            {{ rdo.title }}
+          </el-radio-button>
+        </template>
+        <template v-else>
+          <el-radio
+            v-for="(rdo, rid) in item.options"
+            v-bind="rdo.attrs"
+            v-on="rdo.events"
+            :key="rid"
+          >
+            {{ rdo.title }}
+          </el-radio>
+        </template>
       </el-radio-group>
       <el-checkbox-group
         v-else-if="item.type === 'checkbox'"
         v-model="appForm[item.key]"
       >
-        <el-checkbox
-          v-for="(cbx, cid) in item.options"
-          v-bind="cbx.attr"
-          :key="cid"
-        >
-          {{ cbx.title }}
-        </el-checkbox>
+        <template v-if="item.isButton">
+          <el-checkbox-button
+            v-for="(cbx, cid) in item.options"
+            v-bind="cbx.attrs"
+            v-on="cbx.events"
+            :key="cid"
+          >
+            {{ cbx.title }}
+          </el-checkbox-button>
+        </template>
+        <template v-else>
+          <el-checkbox
+            v-for="(cbx, cid) in item.options"
+            v-bind="cbx.attrs"
+            v-on="cbx.events"
+            :key="cid"
+          >
+            {{ cbx.title }}
+          </el-checkbox>
+        </template>
       </el-checkbox-group>
+      <el-input
+        v-else-if="item.type === 'input'"
+        v-model="appForm[item.key]"
+        v-bind="item.formAttrs"
+        v-on="item.formEvents"
+      >
+        <template v-if="item.formSlot && item.formSlot.prefix" #prefix>
+          <slot :name="item.formSlot.prefix" />
+        </template>
+        <template v-if="item.formSlot && item.formSlot.suffix" #suffix>
+          <slot :name="item.formSlot.suffix" />
+        </template>
+        <template v-if="item.formSlot && item.formSlot.prepend" #prepend>
+          <slot :name="item.formSlot.prepend" />
+        </template>
+        <template v-if="item.formSlot && item.formSlot.append" #append>
+          <slot :name="item.formSlot.append" />
+        </template>
+      </el-input>
+      <el-autocomplete
+        v-else-if="item.type === 'autocomplete'"
+        v-model="appForm[item.key]"
+        v-bind="item.formAttrs"
+        v-on="item.formEvents"
+      />
       <el-input-number
         v-else-if="item.type === 'input-number'"
         v-model="appForm[item.key]"
-        v-bind="item.formAttr"
+        v-bind="item.formAttrs"
+        v-on="item.formEvents"
       />
       <el-select
         v-else-if="item.type === 'select'"
         v-model="appForm[item.key]"
-        v-bind="item.formAttr"
+        v-bind="item.formAttrs"
       >
-        <el-option
-          v-for="slt in item.options"
-          v-bind="slt"
-          :key="slt.value"
-        />
+        <template v-if="item.formSlot && item.formSlot.default">
+          <slot :name="item.formSlot.default" />
+        </template>
+        <template v-else>
+          <template v-if="item.isGroup">
+            <el-option-group
+              v-for="(group, gid) in item.options"
+              :key="gid"
+              :label="group.label"
+              :disabled="group.disabled"
+            >
+              <el-option
+                v-for="(slt, sid) in group.options"
+                v-bind="slt"
+                :key="sid"
+              />
+            </el-option-group>
+          </template>
+          <template>
+            <el-option
+              v-for="(slt, sid) in item.options"
+              v-bind="slt"
+              :key="sid"
+            />
+          </template>
+        </template>
+        <template v-if="item.formSlot && item.formSlot.prefix" #prefix>
+          <slot :name="item.formSlot.prefix" />
+        </template>
+        <template v-if="item.formSlot && item.formSlot.empty" #empty>
+          <slot :name="item.formSlot.empty" />
+        </template>
       </el-select>
       <el-cascader
         v-else-if="item.type === 'cascader'"
         v-model="appForm[item.key]"
-        v-bind="item.formAttr"
-      />
+        v-bind="item.formAttrs"
+        v-on="item.formEvents"
+      >
+        <template v-if="item.formSlot && item.formSlot.default" #default="{ node, data }">
+          <slot :name="item.formSlot.default" :node="node" :data="data" />
+        </template>
+        <template v-if="item.formSlot && item.formSlot.empty" #empty>
+          <slot :name="item.formSlot.empty" />
+        </template>
+      </el-cascader>
+      <el-cascader-panel
+        v-else-if="item.type === 'cascaderPanel'"
+        v-model="appForm[item.key]"
+        v-bind="item.formAttrs"
+        v-on="item.formEvents"
+      >
+        <template v-if="item.formSlot && item.formSlot.default" #default="{ node, data }">
+          <slot :name="item.formSlot.default" :node="node" :data="data" />
+        </template>
+      </el-cascader-panel>
       <el-switch
         v-else-if="item.type === 'switch'"
         v-model="appForm[item.key]"
-        v-bind="item.formAttr"
+        v-bind="item.formAttrs"
+        v-on="item.formEvents"
       />
       <el-slider
         v-else-if="item.type === 'slider'"
         v-model="appForm[item.key]"
-        v-bind="item.formAttr"
+        v-bind="item.formAttrs"
+        v-on="item.formEvents"
       />
       <el-time-picker
         v-else-if="item.type === 'timePicker'"
         v-model="appForm[item.key]"
-        v-bind="item.formAttr"
+        v-bind="item.formAttrs"
       />
       <el-time-select
         v-else-if="item.type === 'timeSelect'"
         v-model="appForm[item.key]"
-        v-bind="item.formAttr"
+        v-bind="item.formAttrs"
+        v-on="item.formEvents"
       />
       <el-date-picker
         v-else-if="item.type === 'datePicker'"
         v-model="appForm[item.key]"
-        v-bind="item.formAttr"
-      />
+        v-bind="item.formAttrs"
+        v-on="item.formEvents"
+      >
+        <template v-if="item.formSlot && item.formSlot.rangeSeparator" #range-separator>
+          <slot :name="item.formSlot.rangeSeparator" />
+        </template>
+      </el-date-picker>
       <el-rate
         v-else-if="item.type === 'rate'"
         v-model="appForm[item.key]"
-        v-bind="item.formAttr"
+        v-bind="item.formAttrs"
+        v-on="item.formEvents"
       />
       <el-color-picker
         v-else-if="item.type === 'colorPicker'"
         v-model="appForm[item.key]"
-        v-bind="item.formAttr"
+        v-bind="item.formAttrs"
+        v-on="item.formEvents"
       />
       <el-transfer
         v-else-if="item.type === 'transfer'"
         v-model="appForm[item.key]"
-        v-bind="item.formAttr"
-      />
+        v-bind="item.formAttrs"
+        v-on="item.formEvents"
+      >
+        <template v-if="item.formSlot && item.formSlot.default" #default="{ option }">
+          <slot :name="item.formSlot.default" :option="option" />
+        </template>
+      </el-transfer>
       <el-upload
         v-else-if="item.type === 'upload'"
         v-model="appForm[item.key]"
-        v-bind="item.formAttr"
+        v-bind="item.formAttrs"
       >
-        <slot name="upload" />
-        <template #tip>
-          <slot name="upload-tip" />
+        <template v-if="item.formSlot && item.formSlot.default">
+          <slot :name="item.formSlot.default" />
+        </template>
+        <template v-else>
+          <el-button size="small" type="primary">点击上传</el-button>
+        </template>
+        <template v-if="item.formSlot && item.formSlot.tip" #tip>
+          <slot :name="item.formSlot.tip" />
+        </template>
+        <template v-if="item.formSlot && item.formSlot.trigger" #trigger>
+          <slot :name="item.formSlot.trigger" />
         </template>
       </el-upload>
     </el-form-item>
@@ -148,8 +264,13 @@ export default defineComponent({
       console.log('submit!', appForm)
     }
 
+    // 初始化表单数据
+    appConfig.formList = appConfig.formList.map(item => {
+      if (!item.formEvents) item.formEvents = {}
+      return item
+    })
     // 初始化表单 model
-    appConfig.formList.forEach(item => appForm[item.key] = item.formAttr && item.formAttr.value ? item.formAttr.value : {})
+    appConfig.formList.forEach(item => appForm[item.key] = item.formAttrs && item.formAttrs.modelValue ? item.formAttrs.modelValue : null)
 
     return {
       appConfig,
--- src/views/demo/form.vue
+++ src/views/demo/form.vue
@@ -32,7 +32,7 @@ export default defineComponent({
   },
   setup() {
     const formConfig = reactive({
-      formAttr: {
+      formAttrs: {
         inline: false,
         labelPosition: 'top',
         labelWidth: 'auto',
@@ -40,101 +40,119 @@ export default defineComponent({
       },
       formList: [
         {
-          type: 'input',
-          key: 'username',
-          labelAttr: {
-            label: '用户名'
-          },
-          formAttr: {
-            type: 'text',
-            modelValue: 'Jaxson',
-            placeholder: '请输入用户名',
-            clearable: true
-          }
-        },
-        {
           type: 'radio',
           key: 'radio',
-          labelAttr: {
+          labelAttrs: {
             label: '单选集合'
           },
-          formAttr: {
+          formAttrs: {
             modelValue: 1
           },
+          formEvents: {},
           options: [
             {
               title: '选项1',
-              attr: {
+              attrs: {
                 label: 0,
                 disabled: true
-              }
+              },
+              events: {}
             },
             {
               title: '选项2',
-              attr: {
+              attrs: {
                 label: 1
               }
             },
             {
               title: '选项3',
-              attr: {
+              attrs: {
                 label: 2
               }
             }
-          ]
+          ],
+          isButton: true
         },
         {
           type: 'checkbox',
           key: 'checkbox',
-          labelAttr: {
+          labelAttrs: {
             label: '多选集合'
           },
-          formAttr: {
+          formAttrs: {
             modelValue: [0, 1]
           },
+          formEvents: {},
           options: [
             {
               title: '选项1',
-              attr: {
+              attrs: {
                 label: 0
               }
             },
             {
               title: '选项2',
-              attr: {
+              attrs: {
                 label: 1
               }
             },
             {
               title: '选项3',
-              attr: {
+              attrs: {
                 label: 2
               }
             }
-          ]
+          ],
+          isButton: true
+        },
+        {
+          type: 'input',
+          key: 'username',
+          labelAttrs: {
+            label: '用户名'
+          },
+          formAttrs: {
+            type: 'text',
+            modelValue: 'Jaxson',
+            placeholder: '请输入用户名',
+            clearable: true
+          },
+          formEvents: {},
+          formSlot: {
+            prefix: 'prefix',
+            suffix: 'suffix',
+            prepend: 'prepend',
+            append: 'append'
+          }
         },
         {
           type: 'input-number',
           key: 'inputNumber',
-          labelAttr: {
+          labelAttrs: {
             label: '计数器'
           },
-          formAttr: {
+          formAttrs: {
             modelValue: 1,
             min: 1,
             max: 10,
             label: '描述说明'
-          }
+          },
+          formEvents: {}
         },
         {
           type: 'select',
           key: 'select',
-          labelAttr: {
+          labelAttrs: {
             label: '选择器'
           },
-          formAttr: {
+          formAttrs: {
             modelValue: 0,
             placeholder: '请选择选项'
+          },
+          formEvents: {},
+          formSlot: {
+            prefix: 'prefix',
+            empty: 'empty'
           },
           options: [
             {
@@ -152,12 +170,61 @@ export default defineComponent({
           ]
         },
         {
+          type: 'select',
+          key: 'selectGroup',
+          labelAttrs: {
+            label: '选择器'
+          },
+          formAttrs: {
+            modelValue: 0,
+            placeholder: '请选择选项'
+          },
+          formEvents: {},
+          options: [
+            {
+              label: '热门城市',
+              options: [
+                {
+                  value: 'Shanghai',
+                  label: '上海'
+                },
+                {
+                  value: 'Beijing',
+                  label: '北京'
+                }
+              ]
+            },
+            {
+              label: '城市名',
+              options: [
+                {
+                  value: 'Chengdu',
+                  label: '成都'
+                },
+                {
+                  value: 'Shenzhen',
+                  label: '深圳'
+                },
+                {
+                  value: 'Guangzhou',
+                  label: '广州'
+                },
+                {
+                  value: 'Dalian',
+                  label: '大连'
+                }
+              ]
+            }
+          ],
+          isGroup: true
+        },
+        {
           type: 'cascader',
           key: 'cascader',
-          labelAttr: {
+          labelAttrs: {
             label: '联级选择器'
           },
-          formAttr: {
+          formAttrs: {
             modelValue: [],
             placeholder: '请选择选项',
             options: [
@@ -428,25 +495,307 @@ export default defineComponent({
                 ]
               }
             ]
-          }
+          },
+          formEvents: {}
+        },
+        {
+          type: 'cascaderPanel',
+          key: 'cascaderPanel',
+          labelAttrs: {
+            label: '联级选择器'
+          },
+          formAttrs: {
+            modelValue: [],
+            placeholder: '请选择选项',
+            options: [
+              {
+                value: 'zhinan',
+                label: '指南',
+                children: [
+                  {
+                    value: 'shejiyuanze',
+                    label: '设计原则',
+                    children: [
+                      {
+                        value: 'yizhi',
+                        label: '一致'
+                      },
+                      {
+                        value: 'fankui',
+                        label: '反馈'
+                      },
+                      {
+                        value: 'xiaolv',
+                        label: '效率'
+                      },
+                      {
+                        value: 'kekong',
+                        label: '可控'
+                      }
+                    ]
+                  },
+                  {
+                    value: 'daohang',
+                    label: '导航',
+                    children: [
+                      {
+                        value: 'cexiangdaohang',
+                        label: '侧向导航'
+                      },
+                      {
+                        value: 'dingbudaohang',
+                        label: '顶部导航'
+                      }
+                    ]
+                  }
+                ]
+              },
+              {
+                value: 'zujian',
+                label: '组件',
+                children: [
+                  {
+                    value: 'basic',
+                    label: 'Basic',
+                    children: [
+                      {
+                        value: 'layout',
+                        label: 'Layout 布局'
+                      },
+                      {
+                        value: 'color',
+                        label: 'Color 色彩'
+                      },
+                      {
+                        value: 'typography',
+                        label: 'Typography 字体'
+                      },
+                      {
+                        value: 'icon',
+                        label: 'Icon 图标'
+                      },
+                      {
+                        value: 'button',
+                        label: 'Button 按钮'
+                      }
+                    ]
+                  },
+                  {
+                    value: 'form',
+                    label: 'Form',
+                    children: [
+                      {
+                        value: 'radio',
+                        label: 'Radio 单选框'
+                      },
+                      {
+                        value: 'checkbox',
+                        label: 'Checkbox 多选框'
+                      },
+                      {
+                        value: 'input',
+                        label: 'Input 输入框'
+                      },
+                      {
+                        value: 'input-number',
+                        label: 'InputNumber 计数器'
+                      },
+                      {
+                        value: 'select',
+                        label: 'Select 选择器'
+                      },
+                      {
+                        value: 'cascader',
+                        label: 'Cascader 级联选择器'
+                      },
+                      {
+                        value: 'switch',
+                        label: 'Switch 开关'
+                      },
+                      {
+                        value: 'slider',
+                        label: 'Slider 滑块'
+                      },
+                      {
+                        value: 'time-picker',
+                        label: 'TimePicker 时间选择器'
+                      },
+                      {
+                        value: 'date-picker',
+                        label: 'DatePicker 日期选择器'
+                      },
+                      {
+                        value: 'datetime-picker',
+                        label: 'DateTimePicker 日期时间选择器'
+                      },
+                      {
+                        value: 'upload',
+                        label: 'Upload 上传'
+                      },
+                      {
+                        value: 'rate',
+                        label: 'Rate 评分'
+                      },
+                      {
+                        value: 'form',
+                        label: 'Form 表单'
+                      }
+                    ]
+                  },
+                  {
+                    value: 'data',
+                    label: 'Data',
+                    children: [
+                      {
+                        value: 'table',
+                        label: 'Table 表格'
+                      },
+                      {
+                        value: 'tag',
+                        label: 'Tag 标签'
+                      },
+                      {
+                        value: 'progress',
+                        label: 'Progress 进度条'
+                      },
+                      {
+                        value: 'tree',
+                        label: 'Tree 树形控件'
+                      },
+                      {
+                        value: 'pagination',
+                        label: 'Pagination 分页'
+                      },
+                      {
+                        value: 'badge',
+                        label: 'Badge 标记'
+                      }
+                    ]
+                  },
+                  {
+                    value: 'notice',
+                    label: 'Notice',
+                    children: [
+                      {
+                        value: 'alert',
+                        label: 'Alert 警告'
+                      },
+                      {
+                        value: 'loading',
+                        label: 'Loading 加载'
+                      },
+                      {
+                        value: 'message',
+                        label: 'Message 消息提示'
+                      },
+                      {
+                        value: 'message-box',
+                        label: 'MessageBox 弹框'
+                      },
+                      {
+                        value: 'notification',
+                        label: 'Notification 通知'
+                      }
+                    ]
+                  },
+                  {
+                    value: 'navigation',
+                    label: 'Navigation',
+                    children: [
+                      {
+                        value: 'menu',
+                        label: 'NavMenu 导航菜单'
+                      },
+                      {
+                        value: 'tabs',
+                        label: 'Tabs 标签页'
+                      },
+                      {
+                        value: 'breadcrumb',
+                        label: 'Breadcrumb 面包屑'
+                      },
+                      {
+                        value: 'dropdown',
+                        label: 'Dropdown 下拉菜单'
+                      },
+                      {
+                        value: 'steps',
+                        label: 'Steps 步骤条'
+                      }
+                    ]
+                  },
+                  {
+                    value: 'others',
+                    label: 'Others',
+                    children: [
+                      {
+                        value: 'dialog',
+                        label: 'Dialog 对话框'
+                      },
+                      {
+                        value: 'tooltip',
+                        label: 'Tooltip 文字提示'
+                      },
+                      {
+                        value: 'popover',
+                        label: 'Popover 弹出框'
+                      },
+                      {
+                        value: 'card',
+                        label: 'Card 卡片'
+                      },
+                      {
+                        value: 'carousel',
+                        label: 'Carousel 走马灯'
+                      },
+                      {
+                        value: 'collapse',
+                        label: 'Collapse 折叠面板'
+                      }
+                    ]
+                  }
+                ]
+              },
+              {
+                value: 'ziyuan',
+                label: '资源',
+                children: [
+                  {
+                    value: 'axure',
+                    label: 'Axure Components'
+                  },
+                  {
+                    value: 'sketch',
+                    label: 'Sketch Templates'
+                  },
+                  {
+                    value: 'jiaohu',
+                    label: '组件交互文档'
+                  }
+                ]
+              }
+            ]
+          },
+          formEvents: {}
         },
         {
           type: 'switch',
           key: 'switch',
-          labelAttr: {
+          labelAttrs: {
             label: '开关'
           },
-          formAttr: {
+          formAttrs: {
             modelValue: false
-          }
+          },
+          formEvents: {}
         },
         {
           type: 'slider',
           key: 'slider',
-          labelAttr: {
+          labelAttrs: {
             label: '滑块'
           },
-          formAttr: {
+          formAttrs: {
             modelValue: [30, 60],
             marks: {
               0: '0°C',
@@ -459,83 +808,90 @@ export default defineComponent({
                 label: '50%'
               }
             }
-          }
+          },
+          formEvents: {}
         },
         {
           type: 'timePicker',
           key: 'timePicker',
-          labelAttr: {
+          labelAttrs: {
             label: '时间选择器1'
           },
-          formAttr: {
+          formAttrs: {
             modelValue: new Date()
-          }
+          },
+          formEvents: {}
         },
         {
           type: 'timeSelect',
           key: 'timeSelect',
-          labelAttr: {
+          labelAttrs: {
             label: '时间选择器2'
           },
-          formAttr: {
+          formAttrs: {
             modelValue: '08:30',
             start: '08:30',
             step: '00:15',
             end: '18:30',
             placeholder: '选择时间'
-          }
+          },
+          formEvents: {}
         },
         {
           type: 'datePicker',
           key: 'datePicker1',
-          labelAttr: {
+          labelAttrs: {
             label: '日期选择器1'
           },
-          formAttr: {
+          formAttrs: {
             modelValue: '',
             type: 'date',
             placeholder: '选择日期'
-          }
+          },
+          formEvents: {}
         },
         {
           type: 'datePicker',
           key: 'datePicker2',
-          labelAttr: {
+          labelAttrs: {
             label: '日期选择器2'
           },
-          formAttr: {
+          formAttrs: {
             modelValue: '',
             type: 'datetime',
             placeholder: '选择日期'
-          }
+          },
+          formEvents: {}
         },
         {
           type: 'rate',
           key: 'rate',
-          labelAttr: {
+          labelAttrs: {
             label: '评分'
           },
-          formAttr: {
+          formAttrs: {
             modelValue: null
-          }
+          },
+          formEvents: {}
         },
         {
           type: 'colorPicker',
           key: 'colorPicker',
-          labelAttr: {
+          labelAttrs: {
             label: '颜色选择器'
           },
-          formAttr: {
+          formAttrs: {
             modelValue: ''
-          }
+          },
+          formEvents: {}
         },
         {
           type: 'transfer',
           key: 'transfer',
-          labelAttr: {
+          labelAttrs: {
             label: '穿梭框'
           },
-          formAttr: {
+          formAttrs: {
             modelValue: [],
             data: [
               {
@@ -555,17 +911,19 @@ export default defineComponent({
                 label: '备选项4'
               }
             ]
-          }
+          },
+          formEvents: {}
         },
         {
           type: 'upload',
           key: 'upload',
-          labelAttr: {
+          labelAttrs: {
             label: '上传文件'
           },
-          formAttr: {
+          formAttrs: {
             action: 'https://jsonplaceholder.typicode.com/posts/'
-          }
+          },
+          formEvents: {}
         }
       ]
     })
